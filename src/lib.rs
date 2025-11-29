8a168c9011c3c0dfe838d0826c54646c1b8c7690
Florian Plattner <florian@lasernoises.com>
2025-11-29 18:27:30 +0100

    Exit on 'q'

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use ratatui::{buffer::Buffer, layout::Rect};
 pub use widget::*;
 
 use color_eyre::Result;
-use crossterm::event::{self, Event};
+use crossterm::event::{self, Event, KeyCode, KeyEvent};
 
 pub fn run<S: 'static>(widget: &mut impl for<'a> FnMut(Pass<'a>) -> PassReturn<S>) -> Result<()> {
     let mut state = init(widget);
@@ -17,7 +17,13 @@ pub fn run<S: 'static>(widget: &mut impl for<'a> FnMut(Pass<'a>) -> PassReturn<S
         terminal.draw(|frame| {
             draw(widget, &mut state, frame.area(), frame.buffer_mut());
         })?;
-        if matches!(event::read()?, Event::Key(_)) {
+        if matches!(
+            event::read()?,
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('q'),
+                ..
+            })
+        ) {
             break;
         }
     }
