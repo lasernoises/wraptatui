be78d6cf780ed8c9f9c7bb2be41adc3cf31b2625
Florian Plattner <florian@lasernoises.com>
2025-11-30 10:20:43 +0100

    Reorder imports

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ pub mod list_content;
 pub mod widget;
 pub mod widgets;
 
-use ratatui::{buffer::Buffer, layout::Rect};
-pub use widget::*;
-
 use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEvent};
+use ratatui::{buffer::Buffer, layout::Rect};
+
+pub use widget::*;
 
 pub fn run<S: 'static>(widget: &mut impl for<'a> FnMut(Pass<'a>) -> PassReturn<S>) -> Result<()> {
     let mut state = init(widget);
