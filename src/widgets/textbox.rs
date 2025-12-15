c2790803535c3dff14fa4579e3cf0cc5ca0f49b5
Florian Plattner <florian@lasernoises.com>
2025-12-15 23:30:42 +0100

    Remove mode from textbox widget

--- src/widgets/textbox.rs
+++ src/widgets/textbox.rs
@@ -1,4 +1,3 @@
-use crossterm::event::KeyCode;
 use ratatui::{
     layout::Position,
     widgets::{Paragraph, Widget},
@@ -9,48 +8,27 @@ use crate::{Pass, PassReturn};
 
 pub use tui_input::Input;
 
-enum Mode {
-    Normal,
-    Insert,
-}
-
 pub fn textbox<'a>(
     pass: Pass<'a>,
     input: &mut Input,
 ) -> PassReturn<'a, impl Sized + use<> + 'static> {
     pass.apply(
         input,
-        |_| Mode::Normal,
-        |input, mode, area, buffer| {
+        |_| (),
+        |input, _, area, buffer| {
             let width = area.width;
             let scroll = input.visual_scroll(width as usize);
 
             Paragraph::new(input.value()).render(area, buffer);
 
-            if let Mode::Insert = mode {
-                let x = input.visual_cursor().max(scroll) - scroll;
+            let x = input.visual_cursor().max(scroll) - scroll;
 
-                Some(Position::new(area.x + x as u16, area.y))
-            } else {
-                None
-            }
+            Some(Position::new(area.x + x as u16, area.y))
         },
-        |input, mode, event| match mode {
-            Mode::Normal => match event.code {
-                KeyCode::Char('i') => {
-                    *mode = Mode::Insert;
-                    true
-                }
-                _ => false,
-            },
-            Mode::Insert => {
-                if event.code == KeyCode::Esc {
-                    *mode = Mode::Normal;
-                } else {
-                    input.handle_event(&crossterm::event::Event::Key(event));
-                }
-                true
-            }
+        |input, _, event| {
+            input
+                .handle_event(&crossterm::event::Event::Key(event))
+                .is_some()
         },
     )
 }
